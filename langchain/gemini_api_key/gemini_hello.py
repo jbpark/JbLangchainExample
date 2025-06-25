25c9c5743ed1e477f76b9d6796862e52aa1f1bcc
jbpark <javata@nate.com>
2025-06-25 22:59:41 +0900

    add gemini

--- langchain/gemini_api_key/gemini_hello.py
+++ langchain/gemini_api_key/gemini_hello.py
@@ -1,11 +1,11 @@
-import google.generativeai as genai
+from google import genai
 
 from _settings.config import GEMINI_KEY
 
-genai.configure(api_key=GEMINI_KEY)  # 발급받은 API 키 입력
+client = genai.Client(api_key=GEMINI_KEY)
 
-model = genai.GenerativeModel("gemini-2.0-flash")
-
-response = model.generate_content("한국의 수도는 어디인가요?")
-
+response = client.models.generate_content(
+    model="gemini-2.0-flash",  # or "gemini-1.5-pro" or other available models
+    contents="한국의 수도는 어디인가요?"
+)
 print(response.text)
